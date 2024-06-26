980edc7a5a917f2b22e0ecacc35765202bb1b3ce
Tarun Sharma <tarunsharma0588@gmail.com>
2024-06-26 22:38:15 +0530

    initial

---
+++ app.js
@@ -0,0 +1,11 @@
+// Function to reverse string
+function reverseString(str) {
+     const strRev =  [...str].reverse().join("");
+     console.log(strRev);
+ }
+ 
+ // Function call
+ reverseString("React.js");
+ reverseString("JavaScript");
+ reverseString("TypeScript");
+ 
